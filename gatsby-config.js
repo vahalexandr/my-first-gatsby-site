0b4b205f8366db79158b6db2d0a28fea221b7946
Alexander Vakhrushev <vahal@yandex.ru>
2023-07-25 18:38:52 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My first gatsby site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
